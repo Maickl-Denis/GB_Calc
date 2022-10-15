c75204e40ed37bd4378e03d248dc5e677e3417dc
Maickl-Denis <81251154+Maickl-Denis@users.noreply.github.com>
2022-10-15 16:55:04 +0300

    добавлено меню

---
+++ menu.py
@@ -0,0 +1,70 @@
+import os
+import komplex
+
+def menu() -> int:
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        numbers = numeric()
+        print(f'{numbers = }')
+        if numbers == 0:
+            break
+        elif numbers == 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            akt = action(numbers)
+            if akt == 0:
+                continue
+            elif 0 < akt < 8:
+                print(f'{akt = }')
+            else:
+                print("Такого пункта меню нет")
+        elif numbers == 2:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            akt = action(numbers)
+            if akt == 0:
+                continue
+            elif 0 < akt < 8:
+                komplex.original(akt)
+            else:
+                print("Такого пункта меню нет")
+        else:
+            print("Такого пункта меню нет")
+
+
+def numeric():
+    try:
+        che = int(input("Выберите работу с числами: \n"
+                        "1. Обычные числа\n"
+                        "2. Комплексные\n"
+                        "0. Выход\n"))        
+    except ValueError:
+        print("Вы ввели некоректное значение")
+        return 0
+    else:
+        return che
+        
+
+def action(flag: int):
+    try:
+        if flag == 1:
+            act = int(input("Выберите действие: \n"
+                            "1. Сложение\n"
+                            "2. Вычитание\n"
+                            "3. Умножение\n"
+                            "4. Деление\n"
+                            "5. Ввозведение в степень\n"
+                            "6. Деление на целое\n"
+                            "7. Остаток от деления\n"
+                            "0. Выход\n"))
+        else:
+            act = int(input("Выберите действие: \n"
+                            "1. Сложение\n"
+                            "2. Вычитание\n"
+                            "3. Умножение\n"
+                            "4. Деление\n"
+                            "5. Ввозведение в степень\n"
+                            "0. Выход\n"))
+    except ValueError:
+        print("Вы ввели некоректное значение")
+        return 0
+    else:
+        return act
